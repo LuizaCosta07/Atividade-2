97415dab1d856c24436b2f3297e6e1401b869d30
Marcelo Henrique <marialuizastyles0705@gmail.com>
2021-11-16 15:31:39 -0300

    update

---
+++ HTML2/Untitled-1.bat
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p.ex1{
+    margin-top: 5px
+    </style>
+    </head>
+    <h1>A propriedade do topo da margem </h1>
+    <p>Um paragrafo sem margens especificadas</p>
+   <p Class="exist">Um paragrafo com uma margem superiorde 25 pixel</p>
+    <p> não aguento mais.</p>
+
+    </body>
+    </html>
